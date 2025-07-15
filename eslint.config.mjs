b499d696b2a3277ba78d9a29fa262c7942ced0b7
Dari <darmcq8632@ecsk12.org>
2025-07-15 00:17:06 -0500

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,6 @@
-// @ts-check
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,12 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...tseslint.config(
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-  ),
+  ...compat.extends("next/core-web-vitals"),
 ];
 
 export default eslintConfig;
