e2092be2c1e2c4b5850f950268b5db2ada834e92
Dari <darmcq8632@ecsk12.org>
2025-07-21 22:28:03 -0500

    feat: create initial TenantPage component with placeholder functions

---
+++ app/admin/suites/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Page() {
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            yayyyyyy
+        </div>
+    )
+}
---
+++ app/admin/tenants/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react'
+import {supabase} from '@/app/lib/supabase/client';
+
+const addTenant = () => {
+
+}
+
+const removeTenant = () => {
+
+}
+
+const lease = () => {
+
+}
+
+
+
+export default function TenantPage() {
+    return (
+            <div className={'flex flex-col'}>
+
+            </div>
+    )
+}
